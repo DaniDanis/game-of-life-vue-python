04d6dbb779b9902f1f8b9d86728efaa72d65ce67
Daniel Dantas da Silva <daniel.dantas.98@gmail.com>
2023-01-09 12:54:04 -0300

    Ultima refatorada, agora o codigo ta mais bonito

--- game_of_life_python/game.py
+++ game_of_life_python/game.py
@@ -2,6 +2,42 @@ import time
 import os
 clear = lambda: os.system('clear')
 
+def game_of_life(jogando, tabuleiro_anterior=[]):
+    clear()
+    if not jogando:
+        tabuleiro = [
+            '00X0000000',
+            'X0X0000000',
+            '0XX0000000',
+            '0000000000',
+            '0000000000',]
+    else:
+        tabuleiro = tabuleiro_anterior
+
+    tabuleiro_aux = tabuleiro.copy()
+
+    coluna = 0
+    linha = 0
+    for x in tabuleiro:
+        for y in x:
+            if y == 'X':
+                vizinhos = conta_vizinho(linha, coluna, tabuleiro)
+                if vizinhos <= 1 or vizinhos >=4:
+                    tabuleiro_aux = mata_celula(linha, coluna, tabuleiro_aux)
+                tabuleiro_aux = verifica_se_nasce(linha, coluna, tabuleiro, tabuleiro_aux)
+            coluna += 1
+        coluna = 0
+        linha += 1
+    
+    tabuleiro = tabuleiro_aux.copy()
+    for x in tabuleiro_aux:
+        print(x.replace('0', "\U00002B1B").replace('X', "\U00002B1C"))
+    print("--------------------------")
+    print("Para sair pressione ctrl+C")
+    time.sleep(2)
+    clear()
+    game_of_life(True, tabuleiro)
+
 def _contador(linha, coluna, tabuleiro, vizinhos):
     if tabuleiro[linha][coluna] == 'X':
         vizinhos += 1
@@ -50,7 +86,7 @@ def cria_celula(linha, coluna, tabuleiro):
     tabuleiro[linha] = linha_aux
     return tabuleiro
 
-def valida_nasce(linha, coluna, tabuleiro, tabuleiro_aux):
+def _valida_nasce(linha, coluna, tabuleiro, tabuleiro_aux):
     if not tabuleiro[linha][coluna] == 'X':
         vizinhos = conta_vizinho(linha, coluna, tabuleiro)
         if vizinhos == 3:
@@ -59,80 +95,40 @@ def valida_nasce(linha, coluna, tabuleiro, tabuleiro_aux):
 
 def verifica_se_nasce(linha, coluna, tabuleiro, tabuleiro_aux):
     if linha - 1 < 0:
-        tabuleiro_aux = valida_nasce(linha+1, coluna, tabuleiro, tabuleiro_aux)
+        tabuleiro_aux = _valida_nasce(linha+1, coluna, tabuleiro, tabuleiro_aux)
         if coluna + 1 < 4:
-            tabuleiro_aux = valida_nasce(linha, coluna+1, tabuleiro, tabuleiro_aux)
-            tabuleiro_aux = valida_nasce(linha+1, coluna+1, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha, coluna+1, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha+1, coluna+1, tabuleiro, tabuleiro_aux)
         if coluna - 1 >= 0:
-            tabuleiro_aux = valida_nasce(linha, coluna-1, tabuleiro, tabuleiro_aux)
-            tabuleiro_aux = valida_nasce(linha+1, coluna-1, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha, coluna-1, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha+1, coluna-1, tabuleiro, tabuleiro_aux)
     elif linha + 1 > 4:
-        tabuleiro_aux = valida_nasce(linha-1, coluna, tabuleiro, tabuleiro_aux)
+        tabuleiro_aux = _valida_nasce(linha-1, coluna, tabuleiro, tabuleiro_aux)
         if coluna - 1 >= 0:
-            tabuleiro_aux = valida_nasce(linha, coluna-1, tabuleiro, tabuleiro_aux)
-            tabuleiro_aux = valida_nasce(linha-1, coluna-1, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha, coluna-1, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha-1, coluna-1, tabuleiro, tabuleiro_aux)
         if coluna + 1 < 9:
-            tabuleiro_aux = valida_nasce(linha-1, coluna+1, tabuleiro, tabuleiro_aux)
-            tabuleiro_aux = valida_nasce(linha, coluna+1, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha-1, coluna+1, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha, coluna+1, tabuleiro, tabuleiro_aux)
     else:
+        tabuleiro_aux = _valida_nasce(linha-1, coluna, tabuleiro, tabuleiro_aux)
+        tabuleiro_aux = _valida_nasce(linha+1, coluna, tabuleiro, tabuleiro_aux)
         if coluna - 1 < 0:
-            tabuleiro_aux = valida_nasce(linha-1, coluna+1, tabuleiro, tabuleiro_aux)
-            tabuleiro_aux = valida_nasce(linha-1, coluna, tabuleiro, tabuleiro_aux)
-            tabuleiro_aux = valida_nasce(linha, coluna+1, tabuleiro, tabuleiro_aux)
-            tabuleiro_aux = valida_nasce(linha+1, coluna+1, tabuleiro, tabuleiro_aux)
-            tabuleiro_aux = valida_nasce(linha+1, coluna, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha+1, coluna+1, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha-1, coluna+1, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha, coluna+1, tabuleiro, tabuleiro_aux)
         elif coluna + 1 > 4:
-            tabuleiro_aux = valida_nasce(linha-1, coluna-1, tabuleiro, tabuleiro_aux)
-            tabuleiro_aux = valida_nasce(linha-1, coluna, tabuleiro, tabuleiro_aux)
-            tabuleiro_aux = valida_nasce(linha, coluna-1, tabuleiro, tabuleiro_aux)
-            tabuleiro_aux = valida_nasce(linha+1, coluna-1, tabuleiro, tabuleiro_aux)
-            tabuleiro_aux = valida_nasce(linha+1, coluna, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha+1, coluna-1, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha-1, coluna-1, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha, coluna-1, tabuleiro, tabuleiro_aux)
         else:
-            tabuleiro_aux = valida_nasce(linha-1, coluna+1, tabuleiro, tabuleiro_aux)
-            tabuleiro_aux = valida_nasce(linha-1, coluna-1, tabuleiro, tabuleiro_aux)
-            tabuleiro_aux = valida_nasce(linha-1, coluna, tabuleiro, tabuleiro_aux)
-            tabuleiro_aux = valida_nasce(linha+1 , coluna+1, tabuleiro, tabuleiro_aux)
-            tabuleiro_aux = valida_nasce(linha+1 , coluna-1, tabuleiro, tabuleiro_aux)
-            tabuleiro_aux = valida_nasce(linha+1 , coluna, tabuleiro, tabuleiro_aux)
-            tabuleiro_aux = valida_nasce(linha, coluna+1, tabuleiro, tabuleiro_aux)
-            tabuleiro_aux = valida_nasce(linha, coluna-1, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha+1, coluna+1, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha+1, coluna-1, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha-1, coluna+1, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha-1, coluna-1, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha, coluna+1, tabuleiro, tabuleiro_aux)
+            tabuleiro_aux = _valida_nasce(linha, coluna-1, tabuleiro, tabuleiro_aux)
     return tabuleiro_aux
-
-def game_of_life(jogando, tabuleiro_anterior=[]):
-    clear()
-    if not jogando:
-        tabuleiro = [
-            '00X0000000',
-            'X0X0000000',
-            '0XX0000000',
-            '0000000000',
-            '0000000000',]
-    else:
-        tabuleiro = tabuleiro_anterior
-
-    tabuleiro_aux = tabuleiro.copy()
-
-    coluna = 0
-    linha = 0
-    for x in tabuleiro:
-        for y in x:
-            if y == 'X':
-                vizinhos = conta_vizinho(linha, coluna, tabuleiro)
-                if vizinhos <= 1 or vizinhos >=4:
-                    tabuleiro_aux = mata_celula(linha, coluna, tabuleiro_aux)
-                tabuleiro_aux = verifica_se_nasce(linha, coluna, tabuleiro, tabuleiro_aux)
-            coluna += 1
-        coluna = 0
-        linha += 1
-    
-    tabuleiro = tabuleiro_aux.copy()
-    for x in tabuleiro_aux:
-        print(x.replace('0', "\U00002B1B").replace('X', "\U00002B1C"))
-    print("--------------------------")
-    print("Para sair pressione ctrl+C")
-    time.sleep(2)
-    clear()
-    game_of_life(True, tabuleiro)
 
 
 game_of_life(False,[])
